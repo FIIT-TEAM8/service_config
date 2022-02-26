838676d76a487942ebd4320497b86795cc8561ec
Dominik Horváth <32178114+Dominik1799@users.noreply.github.com>
2022-02-26 14:33:45 +0100

    Update decrypt_env.sh

--- decrypt_env.sh
+++ decrypt_env.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-openssl enc -aes-256-cbc -pbkdf2 -d -in env.tar.gz.enc -pass "pass:${1}" | tar xz
+openssl enc -aes-256-cbc -pbkdf2 -d -in env.tar.gz.enc -pass "pass:${1}" | tar xz --no-overwrite-dir
